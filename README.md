184f688436556fb3fffaffe4fda8b36e44ced9f1
xueshanlinghu <wangzhuang88888888@126.com>
2021-06-23 22:07:59 +0800

    更新成自己能用的一版

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 爬取知识星球，并制作成 PDF 电子书。
+# 爬取知识星球，并制作成 PDF 电子书
 
 
 ## 功能
@@ -7,117 +7,30 @@
 
 ## 效果图
 
-![效果图.png](https://upload-images.jianshu.io/upload_images/5690299-7aac8142d7794a17.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+![image-20210623210003992](assets/image-20210623210003992.png)
 
 ## 用法
 
-```python
-if __name__ == '__main__':
-    start_url = 'https://api.zsxq.com/v1.10/groups/454584445828/topics?scope=digests&count=20'
-    make_pdf(get_data(start_url))
+首先安装必备的包（包里要求的其他连带的包会自动安装）：
+
+```bash
+pip install requests beautifulsoup4 pdfkit
 ```
 
-把 start_url 改为你需要爬取的星球的相应 url 。
+安装 wkhtmltox，[https://wkhtmltopdf.org/downloads.html](https://wkhtmltopdf.org/downloads.html) 。安装后将安装目录下的 bin 目录加入到环境变量。这是用来制作 pdf 用的，可以将 html 格式转为 pdf 格式。
 
-还有安装好 wkhtmltox ，参考后面的「制作 PDF 电子书」。
+接下来请打开 `crawl.py` 文件，修改配置。
 
-## 模拟登陆
+请先登录你有权限查看的星球的账号，进入该星球页面，请使用谷歌浏览器刷新页面，在 `Network` 面板的抓包内容中找到 `topics?...` 这样的请求，返回的是 `json` 内容。
 
-爬取的是网页版知识星球，[https://wx.zsxq.com/dweb/#](https://wx.zsxq.com/dweb/#)。
+将这个包的 `cookie` 部分复制到代码中 `headers` 部分的 `Cookie` 一栏，将这个请求的 `url`，域名为 `api.zsxq.com` 开头的，复制到代码中 `start_url` 的部分，注意，如果你只需要该星球星主的文章，这个网址一定要注意 `scope` 参数（该星球所有文章填写 `all`）：`topics?scope=by_owner&count=20`。然后代码再往下是批量获取该星球所有还是每过一段时间生成一个 pdf 文件，你可以自行选择修改调整。
 
-这个网站并不是依靠 cookie 来判断你是否登录，而是请求头中的 Authorization 字段。
+完成后运行脚本即可：
 
-所以，需要把 Authorization，User-Agent 换成你自己的。（注意 User-Agent 也要换成你自己的）
-
-```python
-headers = {
-        'Cookie':'UM_distinctid=169a0132ecb3b4-01de055f2e9097-36647902-13c680-169a0132ecca18',
-        'Cookie':'sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2216846276920ae3-0e9d710efddd66-10336654-1296000-16846276921bf%22%2C%22%24device_id%22%3A%2216846276920ae3-0e9d710efddd66-10336654-1296000-16846276921bf%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_referrer_host%22%3A%22%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%7D%7D',
-        'Cookie':'zsxq_access_token=EDBA316F-E9A2-11EB-8C7A-C7D96E1E9228',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
-    }
+```bash
+python crawl.py
 ```
 
-## 分析页面
+## 鸣谢
 
-登录成功后，一般我习惯右键、检查或者查看源代码。
-
-但是这个页面比较特殊，它不把内容放到当前地址栏 URL 下，而是通过异步加载（XHR），只要找对接口就可以了。
-
-精华区的接口：[https://api.zsxq.com/v1.10/groups/2421112121/topics?scope=digests&count=20](https://api.zsxq.com/v1.10/groups/2421112121/topics?scope=digests&count=20)
-
-这个接口是最新 20 条数据的，还有后面数据对应不同接口，这就是后面要说的翻页。
-
-![image.png | left | 827x710](https://cdn.yuque.com/yuque/0/2018/png/104735/1528857296860-38ce73bf-5ae7-406e-8462-10d1a298b8d0.png "")
-
-
-## 制作 PDF 电子书
-
-* 安装 wkhtmltox，[https://wkhtmltopdf.org/downloads.html](https://wkhtmltopdf.org/downloads.html) 。安装后将 bin 目录加入到环境变量。
-* 安装相应依赖：pip install pdfkit
-* 这个工具是把 HTML 文档转换为 PDF 格式。根据 HTML 文档中的 h 标签，也就是标题标签来自动提取出目录。
-* 本来精华区是没有标题的，我把每个问题的前 6 个字符当做标题，以此区分不同问题。
-
-
-
-
-
-
-下面进阶完美操作：
-## 爬取图片
-
-很明显，在返回的数据中的 images 键就是图片，只需提取 large 的，即高清的 url 即可。
-
-关键在于将图片标签 img 插入到 HTML 文档。
-
-我使用 BeautifulSoup 操纵 DOM 的方式。
-
-需要注意的是，有可能图片不止一张，所以需要用 for 循环全部迭代出来
-
-```python
-if content.get('images'):
-    soup = BeautifulSoup(html_template, 'html.parser')
-    for img in content.get('images'):
-        url = img.get('large').get('url')
-        img_tag = soup.new_tag('img', src=url)
-        soup.body.append(img_tag)
-        html_img = str(soup)
-        html = html_img.format(title=title, text=text)
-```
-
-## 翻页问题
-
-* [https://api.zsxq.com/v1.10/groups/2421112121/topics?scope=digests&count=20&end\_time=2018-04-12T15%3A49%3A13.443%2B0800](https://api.zsxq.com/v1.10/groups/2421112121/topics?scope=digests&amp;count=20&amp;end_time=2018-04-12T15%3A49%3A13.443%2B0800)
-
-* 路径后面的 end_time 表示加载帖子的最后日期，以此达到翻页。
-
-* 这个 end_time 是经过 url 转义了的，可以通过 urllib.parse.quote 方法进行转义，关键是找出这个 end_time 是从那里来的。
-
-* 经过我细细观察发现：每次请求返回 20 条帖子，最后一条贴子就与下一条链接的 end_time 有关系。
-
-* 例如最后一条帖子的 create_time 是 2018-01-10T11:49:39.668+0800，那么下一条链接的 end\_time 就是 2018-01-10T11:49:39.667+0800，注意，一个 668，一个 667 , 两者相差 1。
-```python
-end_time = create_time[:20]+str(int(create_time[20:23])-1)+create_time[23:]
-```
-
-* 翻页到最后返回的数据是：
-```json
-{"succeeded":true,"resp_data":{"topics":[]}}
-```
-
-故以 `next_page = rsp.json().get('resp_data').get('topics')` 来判断是否有下一页。
-
-## 制作精美 PDF
-
-通过 css 样式来控制字体大小、布局、颜色等，详见 test.css 文件。
-
-再将此文件引入到 options 字段中。
-
-```python
-    options = {
-        "user-style-sheet": "test.css",
-        ...
-        }
-```
-
-## 最难搞的问题是：Old iron, give me a star ! ! !
+这个仓库最开始不是我弄的，鸣谢之前的作者，在他们的基础上我增加了许多防卡死的功能和日志输出，并且已调整成支持时间段获取了。该项目仅供学习交流使用，切莫用于商业用途！
--- crawl.py
+++ crawl.py
@@ -5,86 +5,108 @@ import os
 import pdfkit
 from bs4 import BeautifulSoup
 from urllib.parse import quote
+from time import sleep
+import random
+import datetime
 
-html_template = """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-</head>
-<body>
-<h1>{title}</h1>
-<p>{text}</p>
-</body>
-</html>
-"""
-htmls = []
-num = 0
-def get_data(url):
 
+def get_data(url, headers, before=None, after=None):
+    """
+    before 默认为None，否则请填入内容，格式为：'2021-06-31 21:00'，所有小于等于该时间的才会被获取
+    after 默认为None，否则请填入内容，格式为：'2021-05-27 20:00'，所有大于等于该时间的才会被获取
+    """
     global htmls, num
-        
-    headers = {
-        'Cookie':'UM_distinctid=165ecede306116a-083654f0777282-34607908-13c680-165ecede3075fd',
-        'Cookie':'sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2216846276920ae3-0e9d710efddd66-10336654-1296000-16846276921bf%22%2C%22%24device_id%22%3A%2216846276920ae3-0e9d710efddd66-10336654-1296000-16846276921bf%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_referrer_host%22%3A%22%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%7D%7D',
-        'Cookie':'zsxq_access_token=9540C63F-C4D5-5461-C3ED-9376940BFCAA',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
-    }
     
-    rsp = requests.get(url, headers=headers)
-    with open('test.json', 'w', encoding='utf-8') as f:        # 将返回数据写入 test.json 方便查看
+    i = 0
+    while i < 10:
+        rsp = requests.get(url, headers=headers)
+        if rsp.json().get("succeeded") == False:
+            sleep(0.01)
+            print("访问失败，重来一遍...")
+            rsp = requests.get(url, headers=headers)
+            i += 1
+        else:
+            break
+
+    with open('temp_content.json', 'w', encoding='utf-8') as f:        # 将返回数据写入 temp_content.json 方便查看
         f.write(json.dumps(rsp.json(), indent=2, ensure_ascii=False))
-    
-    with open('test.json', encoding='utf-8') as f:
-        for topic in json.loads(f.read()).get('resp_data').get('topics'):
-            content = topic.get('question', topic.get('talk', topic.get('task', topic.get('solution'))))
-            # print(content)
-            text = content.get('text', '')
-            text = re.sub(r'<[^>]*>', '', text).strip()
-            text = text.replace('\n', '<br>')
-            title = str(num) + text[:9]
-            num += 1
 
-            if content.get('images'):
-                soup = BeautifulSoup(html_template, 'html.parser')
-                for img in content.get('images'):
-                    url = img.get('large').get('url')
-                    img_tag = soup.new_tag('img', src=url)
-                    soup.body.append(img_tag)
-                    html_img = str(soup)
-                    html = html_img.format(title=title, text=text)
-            else:
-                html = html_template.format(title=title, text=text)
+    with open('temp_content.json', encoding='utf-8') as f:
+        all_contents = json.loads(f.read())
+        contents = all_contents.get('resp_data').get('topics')
+        if contents is not None:
+            for topic in contents:
+                create_time = topic.get("create_time", "")
+                if create_time != "":
+                    create_time = create_time[:16].replace("T", " ")
+                    create_time_time = datetime.datetime.strptime(create_time, '%Y-%m-%d %H:%M')
+                    if after is not None:
+                        after_time = datetime.datetime.strptime(after, '%Y-%m-%d %H:%M')
+                        if after_time > create_time_time: continue
+                    if before is not None:
+                        before_time = datetime.datetime.strptime(before, '%Y-%m-%d %H:%M')   
+                        if create_time_time > before_time: continue
 
-            if topic.get('question'):
-                answer = topic.get('answer').get('text', "")
-                soup = BeautifulSoup(html, 'html.parser')
-                answer_tag = soup.new_tag('p')
-                answer_tag.string = answer
-                soup.body.append(answer_tag)
-                html_answer = str(soup)
-                html = html_answer.format(title=title, text=text)
+                content = topic.get('question', topic.get('talk', topic.get('task', topic.get('solution'))))
+                # print(content)
+                text = content.get('text', '')
+                text = re.sub(r'<[^>]*>', '', text).strip()
+                text = text.replace('\n', '<br>')
+                if text != "":
+                    pos = text.find("<br>")
+                    title = str(num) + " " + text[:pos]
+                else:
+                    title = str(num) + "Error: 找不到内容"
+        
+                if content.get('images'):
+                    soup = BeautifulSoup(html_template, 'html.parser')
+                    for img in content.get('images'):
+                        url = img.get('large').get('url')
+                        img_tag = soup.new_tag('img', src=url)
+                        soup.body.append(img_tag)
+                        html_img = str(soup)
+                        html = html_img.format(title=title, text=text, create_time=create_time)
+                else:
+                    html = html_template.format(title=title, text=text, create_time=create_time)
 
-            htmls.append(html)
+                if topic.get('question'):
+                    answer = topic.get('answer').get('text', "")
+                    soup = BeautifulSoup(html, 'html.parser')
+                    answer_tag = soup.new_tag('p')
+                    answer_tag.string = answer
+                    soup.body.append(answer_tag)
+                    html_answer = str(soup)
+                    html = html_answer.format(title=title, text=text, create_time=create_time)
+
+                htmls.append(html)
+
+                num += 1
+        else:
+            print("*" * 16, "访问失败", "*" * 16)
+            print("失败url:", url)
+            print(all_contents)
+            print(rsp.status_code)
+            print("*" * 40)
 
     next_page = rsp.json().get('resp_data').get('topics')
     if next_page:
         create_time = next_page[-1].get('create_time')
         if create_time[20:23] == "000":
-            end_time = create_time[:20]+"999"+create_time[23:]
+            end_time = create_time[:20] + "999" + create_time[23:]
         else :
             res = int(create_time[20:23])-1
-            end_time = create_time[:20]+str(res).zfill(3)+create_time[23:] # zfill 函数补足结果前面的零，始终为3位数
+            end_time = create_time[:20] + str(res).zfill(3) + create_time[23:] # zfill 函数补足结果前面的零，始终为3位数
         end_time = quote(end_time)
         if len(end_time) == 33:
             end_time = end_time[:24] + '0' + end_time[24:]
         next_url = start_url + '&end_time=' + end_time
-        print(next_url)
-        get_data(next_url)
+        print("next_url:", next_url)
+        sleep(random.randint(1, 5) / 100)
+        get_data(next_url, headers, before, after)
 
     return htmls
 
-def make_pdf(htmls):
+def make_pdf(htmls, pdf_filepath="电子书.pdf"):
     html_files = []
     for index, html in enumerate(htmls):
         file = str(index) + ".html"
@@ -93,7 +115,7 @@ def make_pdf(htmls):
             f.write(html)
 
     options = {
-        "user-style-sheet": "test.css",
+        "user-style-sheet": "default.css",
         "page-size": "Letter",
         "margin-top": "0.75in",
         "margin-right": "0.75in",
@@ -107,15 +129,69 @@ def make_pdf(htmls):
         "outline-depth": 10,
     }
     try:
-        pdfkit.from_file(html_files, "电子书.pdf", options=options)
+        print("生成PDF文件中，请耐心等待...")
+        if os.path.exists(pdf_filepath): os.remove(pdf_filepath)
+        pdfkit.from_file(html_files, pdf_filepath, options=options)
     except Exception as e:
-        pass
+        print("生成pdf报错")
+        print(e)
 
-    for file in html_files:
-        os.remove(file)
+    for i in html_files:
+        os.remove(i)
 
     print("已制作电子书在当前目录！")
 
+
 if __name__ == '__main__':
-    start_url = 'https://api.zsxq.com/v1.10/groups/8424258282/topics?scope=digests&count=20'
-    make_pdf(get_data(start_url))
+    # 这个模板是默认的，无需修改
+    html_template = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+<h1>{title}</h1>
+<p>{create_time}</p>
+<p>{text}</p>
+</body>
+</html>
+"""
+
+    # 请先登录你有权限查看的星球的账号，进入该星球页面
+    # 请使用谷歌浏览器刷新页面，在 Network 面板的抓包内容中找到 topics?... 这样的请求，返回的是 json 内容
+    # 将这个包的 cookie 部分复制到 headers 部分的 Cookie 一栏
+    # 将这个请求的 url，域名为 api.zsxq.com 开头的，复制到下面 start_url 的部分
+    headers = {
+        'Cookie':'abtest_env=product; zsxq_access_token=EB72127D-2A94-A794-46FE-8E1D0F151F40_C348130420D15229; sajssdk_2015_cross_new_user=1; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22414445544118248%22%2C%22first_id%22%3A%2217a3722c1df65-0d10035f06881c8-e726559-2073600-17a3722c1e0421%22%2C%22props%22%3A%7B%7D%2C%22%24device_id%22%3A%2217a3722c1df65-0d10035f06881c8-e726559-2073600-17a3722c1e0421%22%7D',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36 SE 2.X MetaSr 1.0'
+    }
+    start_url = 'https://api.zsxq.com/v2/groups/551212824514/topics?scope=by_owner&count=20'
+
+    # 只取大于等于 after ，小于等于 before 的日期时间的文章，可以省略这俩参数，获取所有的历史文章
+    # 下面这里我演示的是一段时间的拆分获取 pdf，可以批量生成多个，用于内容跨度时间长，内容非常多的星球，你可以自己看着改
+    time_period = [
+        ("2021-04-01 00:00", "2021-06-30 23:59"),
+        ("2021-01-01 00:00", "2021-03-31 23:59"),
+        ("2020-10-01 00:00", "2020-12-31 23:59"),
+        ("2020-07-01 00:00", "2020-09-30 23:59"),
+        ("2020-04-01 00:00", "2020-06-30 23:59"),
+        ("2020-01-01 00:00", "2020-03-31 23:59"),
+        ("2019-10-01 00:00", "2019-12-31 23:59"),
+        ("2019-07-01 00:00", "2019-09-30 23:59"),
+        ("2019-04-01 00:00", "2019-06-30 23:59"),
+        ("2019-01-01 00:00", "2019-03-31 23:59"),
+        ("2018-10-01 00:00", "2018-12-31 23:59"),
+        ("2018-07-01 00:00", "2018-09-30 23:59"),
+        ("2018-04-01 00:00", "2018-06-30 23:59"),
+    ]
+    for period in time_period:
+        pdf_filepath = "你的知识星球%s-%s.pdf" % (period[0][:10].replace("-",""), period[1][:10].replace("-",""))
+        htmls = []
+        num = 1
+        make_pdf(get_data(start_url, headers, before=period[1], after=period[0]), pdf_filepath=pdf_filepath)
+    
+    # 如果你想获取该星球的所有内容，请用这几句代码，但当内容较多的时候，生成 pdf 会极慢
+    # htmls = []
+    # num = 1
+    # make_pdf(get_data(start_url, headers))
